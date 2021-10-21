5773ee12296dd62569aec21dff118ec2922530e2
mac <mac@macui-MacBook-Pro-2.local>
2021-10-22 01:04:07 +0900

    django set again page_135 from MAC

--- lyricca/lyricca_app/urls.py
+++ lyricca/lyricca_app/urls.py
@@ -1,8 +1,8 @@
-#lyricca > urls.py
+#lyricca_app > urls.py
 
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
 ]
